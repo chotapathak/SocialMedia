d2b0b5fe4195e222fcb52cea045aabac51393373
Vikash Pathak <vikashpathak@Vikashs-MacBook-Air.local>
2022-10-15 22:01:42 +0530

    Reintialize git

---
+++ frontend/src/components/Card.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Card extends Component {
+  render() {
+    return (
+      <div className='relative w-350 h-190'>
+
+      </div>
+    )
+  }
+}
---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import {GoogleLogin} from 'react-google-login'
+import { useNavigate } from 'react-router-dom'
+import {FcGoogle} from 'react-icons/fc'
+import logo from '../assets/logowhite.png'
+import shareVideo from '../assets/share.mp4'
+const Login = () => {
+
+    const responseGoogle = (response) => {
+        
+    }
+    return (
+        <div className='flex justify-start items-center flex-col h-screen'>
+            <div className='relative w-full h-full'>
+                <video
+                    src={shareVideo}
+                    type='video/mp4'
+                    loop
+                    controls={false}
+                    autoPlay
+                    muted
+                    className='w-full h-full object-cover'
+                    />
+            </div>
+
+
+            <div className='absolute flex flex-col justify-center items-center top-0 right-0 bottom-0 left-0 bg-blackOverlay'>
+                <div className='p-5'>
+                    <img src={logo} width='130px' alt='logo' />
+                </div>
+
+                <div className='shadow-2x1'>
+                 <GoogleLogin
+                  clientId='' 
+                  render={(renderProps) => (
+                    <button 
+                    type='button'
+                    className='bg-mainColor flex justify-center items-center p-3 rounded-lg curser-pointer outline-none'
+                    onClick={renderProps.onClick}
+                    disabled={renderProps.disabled}
+                    >
+                    <FcGoogle className='mr-4'/>
+                    </button>
+                  )}
+                  onSuccess={responseGoogle}
+                  onFailure={responseGoogle}
+                  buttonText='SIgn in' 
+                  cookiePolicy='single_host_origin'/> 
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
