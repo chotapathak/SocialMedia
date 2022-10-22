f25ec67cbc896844165689801acba5d1596ad1f4
vikashpathak <chotapathak12@gmail.com>
2022-10-22 23:43:18 +0530

    Search bar and pins

---
+++ frontend/src/components/Pin.jsx
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+
+const Pin = (props) => {
+    return (
+        <div>
+            Pin
+        </div>
+    )
+};
+
+export default Pin;
--- frontend/src/components/index.jsx
+++ frontend/src/components/index.jsx
@@ -1,5 +1,10 @@
-export {default as Login} from '../components/Login'
 export {default as GoogleAuth} from '../components/GoogleAuth'
-export {default as SideBar} from '../components/SideBar'
-
+export {default as Login} from '../components/Login'
 export {default as UserProfile} from '../components/UserProfile'
+
+export {default as SideBar} from '../components/SideBar'
+export {default as CreatePin} from '../components/CreatePin'
+export {default as Feed} from '../components/Feed'
+export {default as Navbar} from '../components/Navbar'
+export {default as PinDetail} from '../components/PinDetail'
+export {default as Search} from  '../components/Search';
